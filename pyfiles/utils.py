a533b2d136a5f0b677388e79c2978f7ff6a14c14
JoaquinGianan <90005176+JoaquinGianan@users.noreply.github.com>
2023-01-22 20:12:19 -0300

    several name changes

--- pyfiles/utils.py
+++ pyfiles/utils.py
@@ -13,3 +13,9 @@ def display_metrics(y_true, y_pred):
     print(f"Precision: {precision(y_true, y_pred):.3f}")
     print(f"F1 Score: {f1_score(y_true, y_pred):.3f}")
 
+# def display_metrics(y_true, y_pred):
+#     print(f"Confusion Matrix: \n{confusion_matrix(y_true, y_pred)}")
+#     print(f"Accuracy: {accuracy(y_true, y_pred):.3f}")
+#     print(f"Recall: {recall(y_true, y_pred):.3f}")
+#     print(f"Precision: {precision(y_true, y_pred):.3f}")
+#     print(f"F1 Score: {f1_score(y_true, y_pred):.3f}")
